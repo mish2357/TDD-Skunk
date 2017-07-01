70dd042762586e66c10a2d7055ab4f8a849aa4de
mish2357 <mish2357@stthomas.edu>
2017-07-01 14:48:28 -0500

    First testcase for skunkApp

--- src/SkunkApp.java
+++ src/SkunkApp.java
@@ -1,4 +1,28 @@
 
 public class SkunkApp {
 
+	private int numberOfPlayers;
+	private String[] playerNames = new String[2] ;
+	
+	public void getPlayerInfo() {
+		// TODO Auto-generated method stub
+		
+		numberOfPlayers=2;
+		playerNames=new String[numberOfPlayers];
+		playerNames[0]="Player1";
+		playerNames[1]="Player2";
+	}
+	public static void main(String[] args) {
+		
+	}
+	public int getNumberOfPlayers() {
+		
+		
+		return numberOfPlayers;
+	}
+	public String getPlayerName(int playerNumber) {
+		// TODO Auto-generated method stub
+		return playerNames[playerNumber-1];
+	}
+
 }
--- src/TestSkunkApp.java
+++ src/TestSkunkApp.java
@@ -1,12 +1,24 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestSkunkApp {
+	private SkunkApp game;
+
+	@Before
+	public void setup() {
+
+		game = new SkunkApp();
+	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+
+	public void testGetPlayersInfo() {
+		game.getPlayerInfo();
+		assertEquals(2,game.getNumberOfPlayers());
+		assertEquals("Player1",game.getPlayerName(1));
+		assertEquals("Player1",game.getPlayerName(1));
 	}
 
 }
